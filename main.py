cfce880463bc9469eafe822d9f7145a1a34196e3
Timothy Hogue <hoguetimothy12@gmail.com>
2022-08-11 23:52:44 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+
+
+adj1 = input("Enter an adjective ")
+adj2 = input("Enter and adjective ")
+bird = input("Enter a type of bird ")
+room_in_house = input("Enter a room in the house ")
+verb_past_tense = input("Enter a past tense verb ")
+verb = input("Enter a verb ")
+relative_name = input("Enter a relatives name ")
+noun = input("Enter a noun ")
+liquid = input("Enter a type of liquid ")
+verb2 = input("Enter a verb ending in-ing ")
+body = input("Enter part of the body-plural ")
+plural_noun = input("Enter a plural noun ")
+verb3 = input("Enter a verb ending in-ing ")
+noun2 = input("Enter a noun ")
+print()
+
+
+print("It was a " + adj1 + " cold November day. i wolde to the " + adj2  )
+print("smell of " + bird + " roasting in the " + room_in_house +  " downstairs" )
+print("I " + verb_past_tense + " down the stairs to see if i could help " + verb + " the dinner ")
+print("My mom said, see if " + relative_name + " needs a fresh " + noun )
+print("So I carried a tray of glasses full of " + liquid + " into the " + verb2 + " room ")
+print("When i got there, I couldn't believe my " + body )
+print("There were "  +  plural_noun + verb3 + " on the " + noun2 ) 
+
---
+++ main2.py
@@ -0,0 +1,24 @@
+#madlibs random story generator
+from random import randint
+import copy
+
+story = (
+
+)
+
+
+
+
+
+#Create a dictionary of words to use in the story, seperate by type
+
+my_dictionary = {
+    'noun':['bicycle', 'shoe', 'house', 'mountain', 'cat', 'book'],
+    'adjective':['abrupt', 'beautiful', 'amused', 'bored', 'great', 'new'],
+    'verb':['run', 'cut', 'work', 'grasp', 'laugh', 'reverberate', 'sprinkle'],
+    'city name':['Detroit', 'Greenville', 'Baltimore', 'Paris', 'New york', 'Cozumel'],
+    'sport':['Football', 'Baseball', 'Soccer', 'Tennis', 'Swimming', 'Hockey'],
+    'food':['pasta', 'bacon cheese burger', 'roasted duck', 'lemon pepper wings', 'apple pie', 'deez nutz'],
+
+}
+#create a function that selects a random word
